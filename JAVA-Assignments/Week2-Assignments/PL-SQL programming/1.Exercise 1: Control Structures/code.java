441d8dd2d02c6401e6d8479c6032e8b58ab415ba
Lakshmiprasanna21-hub <luckymlpgoud@gmail.com>
2025-06-28 20:53:21 +0530

    Update code.java

--- JAVA-Assignments/Week2-Assignments/PL-SQL programming/1.Exercise 1: Control Structures/code.java
+++ JAVA-Assignments/Week2-Assignments/PL-SQL programming/1.Exercise 1: Control Structures/code.java
@@ -1,1 +1,23 @@
+---The bank wants to apply a discount to loan interest rates for customers above 60 years old.
+  
+  
+  
+  BEGIN
+  -- Loop through all customers who are older than 60
+  FOR cust IN (
+    SELECT CUSTOMER_ID, AGE
+    FROM CUSTOMERS
+    WHERE AGE > 60
+  ) LOOP
+    -- Apply 1% discount to their loan interest rate
+    UPDATE LOANS
+    SET LOAN_INTEREST_RATE = LOAN_INTEREST_RATE - 1
+    WHERE CUSTOMER_ID = cust.CUSTOMER_ID;
 
+    -- Print output message for confirmation
+    DBMS_OUTPUT.PUT_LINE('1% interest discount applied to customer ID: ' || cust.CUSTOMER_ID);
+  END LOOP;
+
+  -- Commit the changes to the database
+  COMMIT;
+END;
