58c551151fcb8df370c5aff1adc85a5ebbd7eff5
Lakshmiprasanna21-hub <luckymlpgoud@gmail.com>
2025-06-29 16:12:55 +0530

    Update code.java

--- JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/ Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup andTeardown Methods in JUnit/code.java
+++ JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/ Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup andTeardown Methods in JUnit/code.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
     }
@@ -14,7 +15,8 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        if (b == 0)
+            throw new IllegalArgumentException("Division by zero");
         return a / b;
     }
 }
